d2005e1906f750c689ac98dbbc30e7c22b8015c2
rosjeljolly <lambunganrosjeljolly@gmail.com>
2017-05-27 15:07:07 +0800

    Home Banner added and sidebar navigation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12">
-            <h2>Welcome to MarkIt.</h2>
-            <p>Connect with your friends - and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</p>
-        </div>
-        
-    </div>
-</div>
+	<div id="welcome">
+		<img src="/uploads/6.jpg" alt="Welcome Banner">
+		<h2> Markit </h2>
+	</div>
+	<div class="container">
+		<div class="col-md-3 col-md-3 sidebar">
+			<ul class="nav nav-sidebar">
+				<p> Categories </p> 
+				<li><a href="#"> #fashion </a></li>
+				<li><a href="#"> #homeliving </a></li>
+				<li><a href="#"> #electronics </a></li>
+				<li><a href="#"> #healthandbeauty</a></li>
+				<li><a href="#"> #babyandtoys </a></li>
+				<li><a href="#"> #sportsandtravel </a></li>
+				<li><a href="#"> #motors </a></li>
+				<li><a href="#"> #others </a></li>
+			</ul>
+		</div>
+		<div class="col-md-9">
+			hello
+		</div>
+	</div>	
 @endsection
+
+@push('styles')
+<link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
+@endpush
 
