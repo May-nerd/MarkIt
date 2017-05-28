2a01eb8429e98d5308896126d64fdcb002e96d84
rosjeljolly <lambunganrosjeljolly@gmail.com>
2017-05-28 12:00:04 +0800

    Pagination updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,8 +23,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $posts = App\Auction::get();
-        // return $posts;
+        $posts = App\Auction::orderby('desc')->paginate(10);
         return view('home', compact('posts'));
     }
+
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,18 +14,14 @@
          <p class="markit-center"><img src="{{ Storage::disk()->url("public/profilepicture/".Auth::user()->profilepicture) }}" class="markit-circle" style="height:106px;width:106px" alt="Avatar">
          </p>
          <hr>
-         <p><i class="fa fa-user fa-fw markit-margin-right markit-text-theme"></i> Maynard Vargas</p>
+         <p><i class="fa fa-pencil fa-fw markit-margin-right markit-text-theme"></i> Designer, UI</p>-
          <p><i class="fa fa-home fa-fw markit-margin-right markit-text-theme"></i> {{ucwords(strtolower(Auth::user()->town)) }}, {{ucwords(strtolower(Auth::user()->province)) }}</p>
-         <p><i class="fa fa-thumbs-up fa-fw markit-margin-right markit-text-theme"></i> Rating: <i class="glyphicon glyphicon-star"></i><i class="glyphicon glyphicon-star"></i><i class="glyphicon glyphicon-star"></i><i class="glyphicon glyphicon-star"></i><i class="glyphicon glyphicon-star-empty"></i></p>
         </div>
 
       </div>
       <br>
-        <div class="form-group">
-          <div class="col-md-6 col-md-offset-4">
-            <a class="btn btn-primary" href="/profile/{{ Auth::user()->username }}/edit">Edit Profile</a>
-          </div>
-        </div>
+
+      <a class="btn btn-primary" href="/profile/{{ Auth::user()->username }}/edit">Edit Profile</button>
       <br/>
       
       <!-- Accordion -->
@@ -99,35 +95,50 @@
           <div class="markit-card-2 markit-round markit-white">
             <div class="markit-container markit-padding">
               <h6 class="markit-opacity"></h6>
-              <p contenteditable="true" class="markit-border markit-padding">Insert tagline with another tag</p>
-              <button type="button" class="markit-button markit-theme"><i class="fa fa-pencil"></i> Add toMarkIt</button> 
+              <p contenteditable="true" class="markit-border markit-padding">Status: Feeling Blue</p>
+              <button type="button" class="markit-button markit-theme"><i class="fa fa-pencil"></i>  Post</button> 
             </div>
           </div>
         </div>
       </div>
-
-      @forelse ($posts as $post)
-        <div class="markit-container markit-card-2 markit-white markit-round markit-margin"><br>
-          <img src='{{ Storage::disk()->url("public/profilepicture/".$post->user->profilepicture)}}' alt="Avatar" class="markit-left markit-circle markit-margin-right user-icon" style="width:60px">
-          <span class="markit-right markit-opacity">1 min</span>
-          <h4>{{$post->user->fname}} {{$post->user->lname}}</h4>
-          <h6>Rating: 5 Immortal Pearls</h6>
-          <hr class="markit-clear">
-          <div class="markit-row-padding">
-            <p class="item-name">Qilin Horns</p>
-            <div class="markit-half">
-              <img class="img-responsive posted-image" src="/uploads/qilin.jpg" alt="Item Photo">
-            </div>
-            <div class="markit-half">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
+      
+      <div class="markit-container markit-card-2 markit-white markit-round markit-margin"><br>
+        <img src="/uploads/minions.png" alt="Avatar" class="markit-left markit-circle markit-margin-right user-icon" style="width:60px">
+        <span class="markit-right markit-opacity">1 min</span>
+        <h4>John Doe</h4>
+        <h6>Rating: 5 Immortal Pearls</h6>
+        <hr class="markit-clear">
+        <div class="markit-row-padding">
+          <p class="item-name">Qilin Horns</p>
+          <div class="markit-half">
+            <img class="img-responsive posted-image" src="/uploads/qilin.jpg" alt="Item Photo">
           </div>
-          <button type="button" class="markit-button markit-theme-d1 mark-btn"><i class="fa fa-check"></i>  Mark<span> (5)</span></button>
-          <button type="button" class="markit-button markit-theme-d1 mark-btn"></i>Go to Auction</button>
+          <div class="markit-half">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
         </div>
-      @empty
-      @endforelse
+        <button type="button" class="markit-button markit-theme-d1 mark-btn"><i class="fa fa-check"></i>  Mark<span> (5)</span></button>
+        <button type="button" class="markit-button markit-theme-d1 mark-btn"></i>Go to Auction</button>
+      </div>
 
+      <div class="markit-container markit-card-2 markit-white markit-round markit-margin"><br>
+        <img src="/uploads/minions.png" alt="Avatar" class="markit-left markit-circle markit-margin-right user-icon" style="width:60px">
+        <span class="markit-right markit-opacity">8 min</span>
+        <h4>Micah Doe</h4>
+        <h6>Rating: 5 Immortal Pearls</h6>
+        <hr class="markit-clear">
+        <div class="markit-row-padding">
+          <p class="item-name">Drop of Minion's Might</p>
+          <div class="markit-half">
+            <img class="img-responsive posted-image" src="/uploads/minions.png" alt="Item Photo">
+          </div>
+          <div class="markit-half">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+        </div>
+        <button type="button" class="markit-button markit-theme-d1 mark-btn"><i class="fa fa-check"></i>  Mark<span> (3)</span></button>
+        <button type="button" class="markit-button markit-theme-d1 mark-btn"></i>Go to Auction</button>
+      </div>
       
     <!-- End Middle Column -->
     </div>
